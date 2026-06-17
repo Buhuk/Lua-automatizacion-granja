c930ae3a7a9e2633b91d15c96b7601ebd893796a
buhuk <buhuk.style@gmail.com>
2026-06-17 19:57:26 +0200

    Unificado fgit en update

--- update.lua
+++ update.lua
@@ -12,20 +12,31 @@ local archivos = {
     "calibracion.lua"
 }
 
+-- Definimos las cabeceras personalizadas que tú quieras
+local misCabeceras = {
+    --["Authorization"] = "Bearer tu_token_secreto_aqui",
+    --["User-Agent"] = "ComputerCraft-Computer-" .. os.computerID(),
+    ["Accept"] = "application/vnd.github.raw+json" -- Útil para la API de GitHub
+}
+
 print("Actualizando proyecto desde GitHub...")
 
 for _, nombreItem in ipairs(archivos) do
     --local url = string.format("https://raw.githubusercontent.com/%s/%s/%s/%s", usuario, repo, rama, nombreItem)
-    local url = string.format("https://raw.githubusercontent.com/%s/%s/refs/heads/%s/%s", usuario, repo, rama, nombreItem)
+    local url = string.format("https://api.github.com/repos/%s/%s/contents/%s", usuario, repo, nombreItem)
     
     -- Si el archivo ya existe en el juego, lo borramos para actualizarlo
     if fs.exists(nombreItem) then
         fs.delete(nombreItem)
     end
-    
-    -- Lo descargamos usando la API HTTP de ComputerCraft
-    print("Descargando: " .. nombreItem)
-    local respuesta = http.get(url)
+
+    -- Creamos la estructura de opciones para la petición HTTP
+    local opciones = {
+        url = url,
+        headers = misCabeceras
+    }
+
+    local respuesta = http.get(opciones)
     if respuesta then
         local contenido = respuesta.readAll()
         respuesta.close()
@@ -33,6 +44,8 @@ for _, nombreItem in ipairs(archivos) do
         local file = fs.open(nombreItem, "w")
         file.write(contenido)
         file.close()
+
+        print("- ".. nombreItem)
     else
         print("Error al descargar " .. nombreItem)
     end
